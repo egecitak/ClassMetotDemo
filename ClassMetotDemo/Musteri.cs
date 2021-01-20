ae2c345b76a0d6a53eb1e9cec8eed14e210ea4d6
Ege Çıtak <egecitak01@gmail.com>
2021-01-20 13:34:20 +0300

    Musteri class

--- ClassMetotDemo/Musteri.cs
+++ ClassMetotDemo/Musteri.cs
@@ -6,5 +6,10 @@ namespace ClassMetotDemo
 {
     class Musteri
     {
+
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public int TC { get; set; }
     }
 }
